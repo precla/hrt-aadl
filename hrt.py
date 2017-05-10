b4106935eba9bd4eba879969a725d5d3bbd5e80c
precla <antonio.prcela@gmail.com>
2017-05-10 10:50:59 +0200

    Using BeautifulSoap instead of regex

--- hrt.py
+++ hrt.py
@@ -3,8 +3,9 @@
 
 import argparse
 import os
-import re
 import requests
+from bs4 import BeautifulSoup
+from slugify import slugify
 
 PARSER = argparse.ArgumentParser(description='Download audio files from radio archive of HRT.')
 
@@ -22,65 +23,79 @@ def main():
     args = PARSER.parse_args()
 
     # just a quick check to see if the user provided an acceptable link
-    if r"http://radio.hrt.hr/arhiva/" in args.u or r"http://radio.hrt.hr/emisija/" in args.u or r"http://radio.hrt.hr/ep/" in args.u:
+    # and that the link was reachable
+    if r"http://radio.hrt.hr/arhiva/" in args.u or r"http://radio.hrt.hr/emisija/" in args.u:
         response_hrt = requests.get(args.u)
+        # 'dirty workaround' to remove the following warning:
+        # [pylint] E1101:Instance of 'LookupDict' has no 'ok' member
+        #pylint: disable=maybe-no-member
+        if response_hrt.status_code != requests.codes.ok:
+            response_hrt.raise_for_status()
+            return
+
         response_hrt = response_hrt.text
+        soup = BeautifulSoup(response_hrt, "html.parser")
+
+        episode_link = []
+        episode_title = []
+
+        # class_="media-heading" -> this excludes 'Najave' (engl.: upcoming/sheduled)
+        # beacuse those don't have a dl link
+        for link in soup.find_all(class_="media-heading"):
+            # link to episode
+            episode_link.append(link.contents[0].attrs['href'])
+            # episode title
+            episode_title.append(link.string)
     else:
         print("This Link is not valid!")
         return
 
-    # Exclude 'Najave' (engl.: upcoming), beacuse those don't have a dl link
-    # "col-md-8" - /arhiva/ , "col-sm-8" - /emisija/
-    # if it is a link to a singel ep only, than dl that one directly and exit
-    if r"http://radio.hrt.hr/arhiva/" in args.u:
-        response_hrt = response_hrt[response_hrt.find("col-md-8") + 8: response_hrt.find("col-sm-4 side-widgets")]
-    elif r"http://radio.hrt.hr/emisija/" in args.u:
-        response_hrt = response_hrt[response_hrt.find("col-sm-8") + 8 : response_hrt.find("col-sm-4 side-widgets")]
-    elif r"http://radio.hrt.hr/ep/" in args.u:
-        link = args.u[19:]
-        grab_file(link, args.d)
-        return
+    # save to the current working directory if the user did not specify a folder and
+    # create the folder if it doesn't exist
+    if not args.d:
+        dl_dir = os.getcwd()
+    else:
+        dl_dir = args.d
+        if not os.path.exists(dl_dir):
+            os.makedirs(dl_dir)
 
-    # prepare regex for /ep/dogodilo-se-na-danasnji-dan-24-04/201535/ and similiar
-    regexcheck = re.compile(r"/ep/\S+/\"")
+    dl_dir = os.path.normpath(dl_dir)
+    dl_dir = dl_dir + "\\"
 
-    # check with regex and save all matches in a list
-    list_of_matches = list(set(regexcheck.findall(response_hrt)))
+    # iterate trought the list of links and send it to grab_file(...) to dl the mp3
+    for (link, title) in zip(episode_link, episode_title):
+        grab_file(link, title, dl_dir)
 
-    # iterate trought list and send it to grab_file to dl the mp3
-    for link in list_of_matches:
-        link = link.replace("\"", "")
-        grab_file(link, args.d)
-
-    #print(list_of_matches)
-
-def grab_file(link, dl_dir):
+def grab_file(link, title, dl_dir):
     """download file for each link"""
     # add prefix so the link is valid
     link = "http://radio.hrt.hr" + link
 
     # find the title and link to download the *.mp3
     response_download = requests.get(link)
-    if response_download.status_code != 200:
+
+    # 'dirty workaround' to remove the following warning:
+    # [pylint] E1101:Instance of 'LookupDict' has no 'ok' member
+    #pylint: disable=maybe-no-member
+    if response_download.status_code != requests.codes.ok:
         print("Error while trying to reach the link")
 
     response_download = response_download.text
 
-    # find track title
-    regexcheck = re.compile(r"track-title\">([\w\s\-]+)", re.UNICODE)
-    track_title = list(set(regexcheck.findall(response_download)))
+    # remove characters that are not supported in file names from the title:
+    track_title = slugify(title)
 
-    # find the link for the *.mp3
-    regexcheck = re.compile(r"href=\"(\S+.mp3)")
-    mp3_link = list(set(regexcheck.findall(response_download)))
-    mp3_link = "http://radio.hrt.hr" + mp3_link[0]
+    # find the link for the *.mp3 -> class="attachment-file"
+    soup = BeautifulSoup(response_download, "html.parser")
+    for link in soup.find_all(class_="download-section"):
+        # file_size = link.contents[3].contents[3].string
+        mp3_link = "http://radio.hrt.hr" + link.contents[3].attrs['href']
 
-    # save to current working directory if user did not specify a directory
-    if not dl_dir:
-        dl_dir = os.getcwd()
+    response_download = requests.get(mp3_link, stream=True)
+    # convert the number of bytes into chunks (pieces)
+    f_size = int(response_download.headers.get('content-length', 0)) / (32*1024)
 
-    dl_dir = os.path.normpath(dl_dir)
-    dl_dir = dl_dir + "\\"
+    print("Downloading: '" + title + "'")
 
     response_download = requests.get(mp3_link)
     with open(dl_dir + track_title[0] + ".mp3", 'wb') as file_dl:
